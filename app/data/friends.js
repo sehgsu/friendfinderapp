7b51d9fa7c8610e60e4bbb0615bacfc7bd451f87
alex <alex.face@live.com>
2019-05-25 10:44:04 -0400

    FINALLY OPERATIONAL

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friends = [
     {
         name: "Jon",
-        photo: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/jon-snow-game-of-thrones-season-8-episode-1-sec-1555086224.jpg?crop=1.00xw:0.668xh;0,0.0369xh&resize=480:*",
+        pic: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/jon-snow-game-of-thrones-season-8-episode-1-sec-1555086224.jpg?crop=1.00xw:0.668xh;0,0.0369xh&resize=480:*",
         scores: [
             "3",
             "5",
@@ -17,7 +17,7 @@ var friends = [
     },
     {
         name: "Bran",
-        photo: "https://cdn1.thr.com/sites/default/files/imagecache/scale_crop_768_433/2019/05/13_helen_sloan_-_hbo-h_2019.jpg",
+        pic: "https://cdn1.thr.com/sites/default/files/imagecache/scale_crop_768_433/2019/05/13_helen_sloan_-_hbo-h_2019.jpg",
         scores: [
             "5",
             "4",
